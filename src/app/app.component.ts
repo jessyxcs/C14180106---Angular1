775e740f80af5fdce9f04a8c4bf66a6645f52c61
tootzie <c14180106@john.petra.ac.id>
2021-02-23 20:32:51 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  turn = true;
+  board = [
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"],
+    ["*","*","*","*","*"]
+  ]
+  barisP1 : number;
+  kolomP1 : number;
+  barisP2 : number;
+  kolomP2 : number;
+
+  attackP1(barisP1: number, kolomP1: number){
+
+  }
+
+  attackP2(barisP2: number, kolomP2: number){
+
+  }
 }
